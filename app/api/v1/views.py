751387cc52c348fa5b5a8fd9374b7d77514c5e06
koech <koechpetersn@gmail.com>
2018-11-30 10:30:41 +0300

    [Finishes #162215977] modified post functionality of creating an incidence

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,32 +1,22 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
+from flask import jsonify, make_response, request, abort
+from app.api.v1.models import IncidentModel, incidents
 
-from app.v1.models import Incident, db
-
-class IncidentResource(Resource):
+class IncidentsResource(Resource, IncidentModel):
 	'''class for handling incidents'''
-	parser = reqparse.RequestParser()
-	parser.add_argument('title',required=True,type=str,help='title(str) is required')
-	parser.add_argument('nature',required=True,type=str,help='nature(str) is required')
-	parser.add_argument('comment',required=True,type=str,help='comment(str) is required')
+	def __init__(self):
+		self.store = IncidentModel()
 
-	@classmethod
-	def post(cls):
-		arguments = IncidentResource.parser.parse_args()
-		title = arguments.get('title')
-		nature = arguments.get('nature')
-		comment = arguments.get('comment')
 
-		# for redflag in getattr(db,'redflags'):
-		# 	if title == redflag['title']:
-		# 		return {
-		# 		'message' : 'Redflag with that title already exists'
-		# 		}, 409
-		
-		incident = Incident(title = title, nature = nature, comment = comment)
-		incident = incident.save()
-		return {
-				'message' : 'Redflag successfully added','incident' : incident
-				}, 201
+	
+	def post(self):
+		item = request.get_json(force=True)
+		title = item["title"]
+		nature = item["nature"]
+		comment = item["comment"]
+		resp = self.store.save_incident(title, nature, comment)
+		return make_response(jsonify({
+			"message" : "Incident successfully captured",
+			"incident" : resp
+			}),201)
 
-	# def get(cls):
-	# 	resp = self
